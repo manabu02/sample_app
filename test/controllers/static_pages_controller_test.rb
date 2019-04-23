ef75c72c2783ffa408fbc97a4a718cd7cbcfb311
EC2 Default User <fruitcamel@gamil.com>
2019-04-23 12:34:53 +0000

    Not fun Ruby

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,7 +5,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_tilte = "Ruby on Rails Tutorial Sample App"
   end
-  
 
   test "should get home" do
     get static_pages_home_url
@@ -22,12 +21,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title","About|#{@base_title}"
+    assert_select "title", "About | #{@base_title}"
   end
   
-  test "should get about" do
-  get static_pages_about_url
-  assert_response :success
-  assert_select "title","Cntact|#{@base_title}"
+  test "should get contact" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title","Contact|#{@base_title}"
   end
 end
