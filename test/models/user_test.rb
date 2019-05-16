6a0e729359cb52f5b0a83b51ea8a0061c9787ed4
EC2 Default User <fruitcamel@gamil.com>
2019-05-16 05:04:39 +0000

    Use SSL and the Puma webserver in production

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,6 +7,12 @@ class UserTest < ActiveSupport::TestCase
                      password: "foobar", password_confirmation: "foobar")
   end
   
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name:  "",
+  end
+
  test "should be valid" do
    assert @user.valid?
  end
